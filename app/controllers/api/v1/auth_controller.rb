232e67f881637de283755742236cb258656bf371
Melindadiaz07 <melindadiaz@me.com>
2020-11-17 20:10:06 -0500

    Added auth/v1 folder with auth_controller and create method, added jwt gem

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::AuthController < ApplicationController
+
+    def create 
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        token = encode("user_id": @user.id)
+        render json: 
+        {
+          authenticated: true,
+          message: "login success",
+          token: token,
+          user: @user,
+          courses: @user.courses
+        }, status: :accepted
+      else
+          render json: {
+          authenticated: false,
+          message: "login failed"
+        }, status: :unauthorized 
+      end
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   resources :replies
   resources :posts
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      post "/signup", to: "users#create"
+      post '/login', to: 'auth#create'
+    end
+  end
 end
