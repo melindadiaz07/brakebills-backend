6bdeb11b99f6861f38e84fa908c0c1cee93bb66a
Melindadiaz07 <melindadiaz@me.com>
2020-11-17 20:34:17 -0500

    Set up template for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+Reply.destroy_all
+Department.destroy_all
+AreaOfStudy.destroy_all
+Faculty.destroy_all
