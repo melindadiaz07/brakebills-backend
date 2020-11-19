ee57417977923801065c72b30f71031a4848fee0
Melindadiaz07 <melindadiaz@me.com>
2020-11-19 11:58:39 -0500

    Seeded db with faker data for users, posts, and replies tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,3 +29,21 @@ end
                 fake_bio: Faker::Movies::Departed.quote, real_bio: Faker::Movies::PrincessBride.quote, area_of_study_id: randArea)
 end
 
+
+5.times do 
+
+  User.create(username: Faker::Internet.email, password: "password", first: Faker::Name.first_name, last:Faker::Games::DnD.monster, admin?: false)
+end
+
+3.times do 
+  randUser = rand((User.first.id)..(User.last.id))
+
+  Post.create(title: Faker::Movies::Hobbit.location, content: Faker::Movies::Hobbit.quote, category: Faker::Job.field, user_id: randUser)
+
+end
+
+7.times do 
+  randPost = rand((Post.first.id)..(Post.last.id))
+
+  Reply.create(replier: Faker::Internet.email, content: Faker::TvShows::RickAndMorty.quote, post_id: randPost)
+end
