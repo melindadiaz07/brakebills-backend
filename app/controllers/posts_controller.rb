cc62c3f8b7eeabfa547dd955d5792bfc30fdf79c
Melindadiaz07 <melindadiaz@me.com>
2020-11-29 15:34:31 -0500

    Updated controllers for posts to render proper json

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all 
-    render json: @posts
+    render json: @posts.to_json(include: [:user, :replies])
   end
 
   def create
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
                         category: params[:category],
                         content: params[:content],
                         user_id: params[:user_id])
-    render json: @post
+    render json: @post.to_json
   end
 
 end
